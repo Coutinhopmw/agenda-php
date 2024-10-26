b081f49287dd40cdc8a3f0c8f948cdc2d9f20250
Coutinhopmw <cassiocoutinho258@gmail.com>
2024-10-25 21:22:18 -0300

    Utilização de seedrs, factories e criação da versão inicial da tela do projeto

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
 
 class category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'titler',
+        'color',
+        'user_id'
+
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
+
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,500;1,500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital@0;1&display=swap" rel="stylesheet">
+
+    <title>Agenda PHP</title>
+</head>
+<body>
+    <div class="conteiner">
+        <div class="sidebar">
+            logo
+        </div>
+        <div class="content">
+            <a href="#" class="btn btn-primary">
+                Criar tarefa
+            </a>
+        </div>
+    </div>
+</body>
+</html>
